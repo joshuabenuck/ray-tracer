ecb05ffcd09d04a8d937972325cae4067d9aa31a
Joshua Benuck <joshua_benuck@yahoo.com>
2020-08-23 16:22:25 -0700

    first pass at Matrix::inverse

--- src/matrix.rs
+++ src/matrix.rs
@@ -21,6 +21,56 @@ impl Index<usize> for Matrix2x2 {
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Matrix3x3([[f64; 3]; 3]);
 
+impl Matrix3x3 {
+    fn submatrix(&self, row: usize, column: usize) -> Matrix2x2 {
+        let mut sub = [[0.0; 2]; 2];
+        let mut sr = 0;
+        for r in 0..3 {
+            if row == r {
+                continue;
+            }
+            let mut sc = 0;
+            for c in 0..3 {
+                if column == c {
+                    continue;
+                }
+                sub[sr][sc] = self.0[r][c];
+                sc += 1;
+            }
+            sr += 1;
+        }
+        Matrix2x2(sub)
+    }
+
+    // determinant of the submatrix
+    fn minor(&self, row: usize, column: usize) -> f64 {
+        let sub = self.submatrix(row, column);
+        sub.determinant()
+    }
+
+    fn cofactor(&self, row: usize, column: usize) -> f64 {
+        /*
+        sign swapped as follows
+        [+ - +]
+        [- + -]
+        [+ - +]
+        or, more simply, if row + column is odd
+        */
+        let sign = if (row + column) % 2 == 0 { 1.0 } else { -1.0 };
+        self.minor(row, column) * sign
+    }
+
+    fn determinant(&self) -> f64 {
+        let mut determinant = 0.0;
+        let mut c = 0;
+        for col in &self.0[0] {
+            determinant += col * self.cofactor(0, c);
+            c += 1;
+        }
+        determinant
+    }
+}
+
 impl Index<usize> for Matrix3x3 {
     type Output = [f64; 3];
 
@@ -29,7 +79,7 @@ impl Index<usize> for Matrix3x3 {
     }
 }
 
-#[derive(Debug, Copy, Clone, PartialEq)]
+#[derive(Debug, Copy, Clone)]
 pub struct Matrix4x4([[f64; 4]; 4]);
 
 impl Matrix4x4 {
@@ -52,6 +102,97 @@ impl Matrix4x4 {
             }
         }
         Matrix4x4(ta)
+    }
+
+    fn submatrix(&self, row: usize, column: usize) -> Matrix3x3 {
+        let mut sub = [[0.0; 3]; 3];
+        let mut sr = 0;
+        for r in 0..4 {
+            if row == r {
+                continue;
+            }
+            let mut sc = 0;
+            for c in 0..4 {
+                if column == c {
+                    continue;
+                }
+                sub[sr][sc] = self.0[r][c];
+                sc += 1;
+            }
+            sr += 1;
+        }
+        Matrix3x3(sub)
+    }
+
+    fn cofactor(&self, row: usize, column: usize) -> f64 {
+        /*
+        sign swapped as follows
+        [+ - +]
+        [- + -]
+        [+ - +]
+        or, more simply, if row + column is odd
+        */
+        let sign = if (row + column) % 2 == 0 { 1.0 } else { -1.0 };
+        self.minor(row, column) * sign
+    }
+
+    fn determinant(&self) -> f64 {
+        let mut determinant = 0.0;
+        let mut c = 0;
+        for col in &self.0[0] {
+            determinant += col * self.cofactor(0, c);
+            c += 1;
+        }
+        determinant
+    }
+
+    fn is_invertiable(&self) -> bool {
+        self.determinant() != 0.0
+    }
+
+    fn inverse(&self) -> Matrix4x4 {
+        let mut cofactors = [[0.0; 4]; 4];
+        for row in 0..4 {
+            for col in 0..4 {
+                cofactors[row][col] = self.cofactor(row, col);
+            }
+        }
+        let cofactors = Matrix4x4(cofactors).transpose();
+        let determinant = self.determinant();
+        let mut inverse = [[0.0; 4]; 4];
+        for row in 0..4 {
+            for col in 0..4 {
+                inverse[row][col] = cofactors[row][col] / determinant;
+            }
+        }
+        Matrix4x4(inverse)
+    }
+
+    // determinant of the submatrix
+    fn minor(&self, row: usize, column: usize) -> f64 {
+        let sub = self.submatrix(row, column);
+        sub.determinant()
+    }
+}
+
+const EPSILON: f64 = 0.00001;
+
+fn equal(a: f64, b: f64) -> bool {
+    f64::abs(a - b) < EPSILON
+}
+
+impl PartialEq for Matrix4x4 {
+    fn eq(&self, other: &Matrix4x4) -> bool {
+        let a = self;
+        let b = other;
+        for row in 0..4 {
+            for col in 0..4 {
+                if !equal(a[row][col], b[row][col]) {
+                    return false;
+                }
+            }
+        }
+        true
     }
 }
 
@@ -221,5 +362,103 @@ mod tests {
     fn matrix_determinant() {
         let a = Matrix2x2([[1.0, 5.0], [-3.0, 2.0]]);
         assert_eq!(a.determinant(), 17.0);
+    }
+
+    #[test]
+    fn matrix_submatrix() {
+        let a = Matrix3x3([[1.0, 5.0, 0.0], [-3.0, 2.0, 7.0], [0.0, 6.0, -3.0]]);
+        let sub_a = Matrix2x2([[-3.0, 2.0], [0.0, 6.0]]);
+        assert_eq!(a.submatrix(0, 2), sub_a);
+
+        let a = Matrix4x4([
+            [-6.0, 1.0, 1.0, 6.0],
+            [-8.0, 5.0, 8.0, 6.0],
+            [-1.0, 0.0, 8.0, 2.0],
+            [-7.0, 1.0, -1.0, 1.0],
+        ]);
+        let sub_a = Matrix3x3([[-6.0, 1.0, 6.0], [-8.0, 8.0, 6.0], [-7.0, -1.0, 1.0]]);
+        assert_eq!(a.submatrix(2, 1), sub_a);
+    }
+
+    #[test]
+    fn matrix_minor() {
+        let a = Matrix3x3([[3.0, 5.0, 0.0], [2.0, -1.0, -7.0], [6.0, -1.0, 5.0]]);
+        let b = a.submatrix(1, 0);
+        assert_eq!(b.determinant(), 25.0);
+        assert_eq!(a.minor(1, 0), 25.0);
+    }
+
+    #[test]
+    fn matrix_cofactor() {
+        let a = Matrix3x3([[3.0, 5.0, 0.0], [2.0, -1.0, -7.0], [6.0, -1.0, 5.0]]);
+        assert_eq!(a.minor(0, 0), -12.0);
+        assert_eq!(a.cofactor(0, 0), -12.0);
+        assert_eq!(a.minor(1, 0), 25.0);
+        assert_eq!(a.cofactor(1, 0), -25.0);
+    }
+
+    #[test]
+    fn matrix_determinant_for_larger() {
+        let a = Matrix3x3([[1.0, 2.0, 6.0], [-5.0, 8.0, -4.0], [2.0, 6.0, 4.0]]);
+        assert_eq!(a.cofactor(0, 0), 56.0);
+        assert_eq!(a.cofactor(0, 1), 12.0);
+        assert_eq!(a.cofactor(0, 2), -46.0);
+        assert_eq!(a.determinant(), -196.0);
+
+        let a = Matrix4x4([
+            [-2.0, -8.0, 3.0, 5.0],
+            [-3.0, 1.0, 7.0, 3.0],
+            [1.0, 2.0, -9.0, 6.0],
+            [-6.0, 7.0, 7.0, -9.0],
+        ]);
+        assert_eq!(a.cofactor(0, 0), 690.0);
+        assert_eq!(a.cofactor(0, 1), 447.0);
+        assert_eq!(a.cofactor(0, 2), 210.0);
+        assert_eq!(a.cofactor(0, 3), 51.0);
+        assert_eq!(a.determinant(), -4071.0);
+    }
+
+    #[test]
+    fn matrix_invertible() {
+        let a = Matrix4x4([
+            [6.0, 4.0, 4.0, 4.0],
+            [5.0, 5.0, 7.0, 6.0],
+            [4.0, -9.0, 3.0, -7.0],
+            [9.0, 1.0, 7.0, -6.0],
+        ]);
+        assert_eq!(a.determinant(), -2120.0);
+        assert_eq!(a.is_invertiable(), true);
+
+        let a = Matrix4x4([
+            [-4.0, 2.0, -2.0, -3.0],
+            [9.0, 6.0, 2.0, 6.0],
+            [0.0, -5.0, 1.0, -5.0],
+            [0.0, 0.0, 0.0, 0.0],
+        ]);
+        assert_eq!(a.determinant(), 0.0);
+        assert_eq!(a.is_invertiable(), false);
+    }
+
+    #[test]
+    fn matrix_invert() {
+        let a = Matrix4x4([
+            [-5.0, 2.0, 6.0, -8.0],
+            [1.0, -5.0, 1.0, 8.0],
+            [7.0, 7.0, -6.0, -7.0],
+            [1.0, -3.0, 7.0, 4.0],
+        ]);
+        let b = a.inverse();
+        assert_eq!(a.determinant(), 532.0);
+        assert_eq!(a.cofactor(2, 3), -160.0);
+        assert_eq!(b[3][2], -160.0 / 532.0);
+        assert_eq!(a.cofactor(3, 2), 105.0);
+        assert_eq!(b[2][3], 105.0 / 532.0);
+        let inverse = Matrix4x4([
+            [0.21805, 0.45113, 0.24060, -0.04511],
+            [-0.80827, -1.45677, -0.44361, 0.52068],
+            [-0.07895, -0.22368, -0.05263, 0.19737],
+            [-0.52256, -0.81391, -0.30075, 0.30639],
+        ]);
+        assert_eq!(b, inverse);
     }
 }
