4cc88cdcc21f9c42696bfa9502ee8fe09d4ae8db
Joshua Benuck <joshua_benuck@yahoo.com>
2020-08-22 17:22:09 -0700

    color, canvas, partial ppm support

--- src/tuple.rs
+++ src/tuple.rs
@@ -1,3 +1,4 @@
+use std::cmp::{max, min};
 use std::ops::{Add, Div, Mul, Neg, Sub};
 
 const EPSILON: f64 = 0.00001;
@@ -174,6 +175,139 @@ impl Div<f64> for Tuple {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+struct Color {
+    red: f64,
+    green: f64,
+    blue: f64,
+}
+
+impl Color {
+    pub fn new(red: f64, green: f64, blue: f64) -> Color {
+        Color { red, green, blue }
+    }
+}
+
+impl From<Color> for (usize, usize, usize) {
+    fn from(color: Color) -> (usize, usize, usize) {
+        let r = (color.red * 255.0).round() as isize;
+        let g = (color.green * 255.0).round() as isize;
+        let b = (color.blue * 255.0).round() as isize;
+        (
+            min(255, max(0, r)) as usize,
+            min(255, max(0, g)) as usize,
+            min(255, max(0, b)) as usize,
+        )
+    }
+}
+
+impl PartialEq for Color {
+    fn eq(&self, other: &Color) -> bool {
+        let a = self;
+        let b = other;
+        equal(a.red, b.red) && equal(a.green, b.green) && equal(a.blue, b.blue)
+    }
+}
+
+impl Add for Color {
+    type Output = Color;
+
+    fn add(self, other: Color) -> Color {
+        Color {
+            red: self.red + other.red,
+            green: self.green + other.green,
+            blue: self.blue + other.blue,
+        }
+    }
+}
+
+impl Sub for Color {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self {
+            red: self.red - other.red,
+            green: self.green - other.green,
+            blue: self.blue - other.blue,
+        }
+    }
+}
+
+impl Mul<f64> for Color {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self {
+        Self {
+            red: self.red * rhs,
+            green: self.green * rhs,
+            blue: self.blue * rhs,
+        }
+    }
+}
+
+/// Hadamard product (or Schur product)
+/// Multiply corresponding components of each color to form a new color.
+impl Mul for Color {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        let a = self;
+        let b = rhs;
+        Self {
+            red: a.red * b.red,
+            green: a.green * b.green,
+            blue: a.blue * b.blue,
+        }
+    }
+}
+
+struct Canvas {
+    width: usize,
+    height: usize,
+    pixels: Vec<Color>,
+}
+
+impl Canvas {
+    fn new(width: usize, height: usize) -> Canvas {
+        let black = Color::new(0.0, 0.0, 0.0);
+        Canvas {
+            width,
+            height,
+            pixels: vec![black; width * height],
+        }
+    }
+
+    fn xy(&self, x: usize, y: usize) -> usize {
+        x + y * self.width
+    }
+
+    fn write_pixel(&mut self, x: usize, y: usize, color: Color) {
+        let idx = self.xy(x, y);
+        self.pixels[idx] = color;
+    }
+
+    fn pixel_at(&self, x: usize, y: usize) -> Color {
+        let idx = self.xy(x, y);
+        self.pixels[idx]
+    }
+
+    fn to_ppm(&self) -> String {
+        let mut ppm = format!("P3\n{} {}\n255\n", self.width, self.height);
+        let width = self.width;
+        for row in self.pixels.chunks(width) {
+            ppm += row
+                .iter()
+                .map(|c| Into::<(usize, usize, usize)>::into(*c))
+                .map(|c| format!("{} {} {}", c.0, c.1, c.2))
+                .collect::<Vec<String>>()
+                .join(" ")
+                .as_str();
+            ppm += "\n";
+        }
+        ppm
+    }
+}
+
 #[test]
 fn tuple_is_point() {
     let a: Tuple = (4.3, -4.2, 3.1, 1.0).into();
@@ -308,4 +442,82 @@ fn vector_cross_product() {
     let b = &Tuple::vector(2.0, 3.0, 4.0);
     assert_eq!(a * b, Tuple::vector(-1.0, 2.0, -1.0));
     assert_eq!(b * a, Tuple::vector(1.0, -2.0, 1.0));
+}
+
+#[test]
+fn color() {
+    let c = Color::new(-0.5, 0.4, 1.7);
+    assert_eq!(c.red, -0.5);
+    assert_eq!(c.green, 0.4);
+    assert_eq!(c.blue, 1.7);
+}
+
+#[test]
+fn color_ops() {
+    let c1 = Color::new(0.9, 0.6, 0.75);
+    let c2 = Color::new(0.7, 0.1, 0.25);
+    assert_eq!(c1 + c2, Color::new(1.6, 0.7, 1.0));
+    assert_eq!(c1 - c2, Color::new(0.2, 0.5, 0.5));
+
+    let c = Color::new(0.2, 0.3, 0.4);
+    assert_eq!(c * 2.0, Color::new(0.4, 0.6, 0.8));
+
+    let c1 = Color::new(1.0, 0.2, 0.4);
+    let c2 = Color::new(0.9, 1.0, 0.1);
+    assert_eq!(c1 * c2, Color::new(0.9, 0.2, 0.04));
+}
+
+#[test]
+fn canvas_create() {
+    let mut c = Canvas::new(10, 20);
+    assert_eq!(c.width, 10);
+    assert_eq!(c.height, 20);
+    let black = Color::new(0.0, 0.0, 0.0);
+    for pixel in &c.pixels {
+        assert_eq!(pixel, &black);
+    }
+
+    let red = Color::new(1.0, 0.0, 0.0);
+    c.write_pixel(2, 3, red);
+    assert_eq!(c.pixel_at(2, 3), red);
+}
+
+#[test]
+fn canvs_to_ppm() {
+    let c = Canvas::new(5, 3);
+    let ppm = c.to_ppm();
+    let first_three = ["P3", "5 3", "255"];
+    assert_eq!(ppm.split("\n").take(3).collect::<Vec<&str>>(), first_three);
+
+    let mut c = Canvas::new(5, 3);
+    let c1 = Color::new(1.5, 0.0, 0.0);
+    let c2 = Color::new(0.0, 0.5, 0.0);
+    let c3 = Color::new(-0.5, 0.0, 1.0);
+    c.write_pixel(0, 0, c1);
+    c.write_pixel(2, 1, c2);
+    c.write_pixel(4, 2, c3);
+    let four_to_six = [
+        "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+        "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0",
+        "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255",
+    ];
+    let ppm = c.to_ppm();
+    assert_eq!(
+        ppm.split("\n").skip(3).take(3).collect::<Vec<&str>>(),
+        four_to_six
+    );
+
+    let mut c = Canvas::new(10, 2);
+    c.pixels = vec![Color::new(1.0, 0.8, 0.6); c.width * c.height];
+    let four_to_seven = [
+        "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204",
+        "153 255 204 153 255 204 153 255 204 153 255 204 153",
+        "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204",
+        "153 255 204 153 255 204 153 255 204 153 255 204 153",
+    ];
+    let ppm = c.to_ppm();
+    assert_eq!(
+        ppm.split("\n").skip(3).take(4).collect::<Vec<&str>>(),
+        four_to_seven
+    );
 }
