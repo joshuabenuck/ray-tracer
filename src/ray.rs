0ada4d76d83a2f7ace7f44962fc5d3beba8dc92a
Joshua Benuck <joshua_benuck@yahoo.com>
2020-08-24 21:03:19 -0700

    intersections with unit spheres

--- src/ray.rs
+++ src/ray.rs
@@ -15,11 +15,59 @@ impl Ray {
     }
 }
 
+#[derive(Debug, Copy, Clone, PartialEq)]
 struct Sphere {}
 
 impl Sphere {
-    fn intersects(&self, r: &Ray) -> Vec<Tuple> {
-        Vec::new()
+    fn intersects(&self, ray: &Ray) -> Vec<Intersection> {
+        let sphere_to_ray = ray.origin - Tuple::point(0.0, 0.0, 0.0);
+
+        let a = ray.direction.dot(&ray.direction);
+        let b = 2.0 * ray.direction.dot(&sphere_to_ray);
+        let c = sphere_to_ray.dot(&sphere_to_ray) - 1.0;
+
+        let discriminant = (b * b) - 4.0 * a * c;
+
+        if discriminant < 0.0 {
+            return Vec::new();
+        }
+
+        let t1 = (-b - discriminant.sqrt()) / (2.0 * a);
+        let t2 = (-b + discriminant.sqrt()) / (2.0 * a);
+        vec![
+            Intersection::new(t1, Sphere {}),
+            Intersection::new(t2, Sphere {}),
+        ]
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Intersection {
+    t: f64,
+    object: Sphere,
+}
+
+impl Intersection {
+    fn new(t: f64, object: Sphere) -> Intersection {
+        Intersection { t, object }
+    }
+}
+
+trait Intersections {
+    fn hit(&self) -> Option<&Intersection>;
+}
+
+impl Intersections for Vec<Intersection> {
+    fn hit(&self) -> Option<&Intersection> {
+        if self.len() == 0 {
+            return None;
+        }
+        for i in self.iter() {
+            if i.t > 0.0 {
+                return Some(i);
+            }
+        }
+        None
     }
 }
 
@@ -49,5 +97,93 @@ mod tests {
     #[test]
     fn ray_sphere_intersection() {
         // a ray intersects a sphere at two points
+        let r = Ray::new(Tuple::point(0.0, 0.0, -5.0), Tuple::vector(0.0, 0.0, 1.0));
+        let s = Sphere {};
+        let xs = s.intersects(&r);
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, 4.0);
+        assert_eq!(xs[0].object, Sphere {});
+        assert_eq!(xs[1].t, 6.0);
+        assert_eq!(xs[1].object, Sphere {});
+
+        // a ray intersects a sphere at a tangent
+        let r = Ray::new(Tuple::point(0.0, 1.0, -5.0), Tuple::vector(0.0, 0.0, 1.0));
+        let xs = s.intersects(&r);
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, 5.0);
+        assert_eq!(xs[1].t, 5.0);
+
+        // a ray misses a sphere
+        let r = Ray::new(Tuple::point(0.0, 2.0, -5.0), Tuple::vector(0.0, 0.0, 1.0));
+        let xs = s.intersects(&r);
+        assert_eq!(xs.len(), 0);
+
+        // a ray originates inside a sphere
+        let r = Ray::new(Tuple::point(0.0, 0.0, 0.0), Tuple::vector(0.0, 0.0, 1.0));
+        let xs = s.intersects(&r);
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, -1.0);
+        assert_eq!(xs[1].t, 1.0);
+
+        // a sphere is behind a ray
+        let r = Ray::new(Tuple::point(0.0, 0.0, 5.0), Tuple::vector(0.0, 0.0, 1.0));
+        let xs = s.intersects(&r);
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, -6.0);
+        assert_eq!(xs[1].t, -4.0);
+    }
+
+    #[test]
+    fn intersection() {
+        // an intersection encapsulates t and object
+        let s = Sphere {};
+        let i = Intersection::new(3.5, s);
+        assert_eq!(3.5, i.t);
+        // assert_eq!(i.object, s);
+
+        // aggregate intersections
+        let s = Sphere {};
+        let i1 = Intersection::new(1.0, s.clone());
+        let i2 = Intersection::new(2.0, s);
+        let mut xs = [i1, i2];
+        xs.sort_by(|a, b| a.t.partial_cmp(&b.t).unwrap());
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, 1.0);
+        assert_eq!(xs[1].t, 2.0);
+
+        // the hit when all intersections have positive t
+        let s = Sphere {};
+        let i1 = Intersection::new(1.0, s.clone());
+        let i2 = Intersection::new(2.0, s.clone());
+        let mut xs = vec![i2, i1];
+        xs.sort_by(|a, b| a.t.partial_cmp(&b.t).unwrap());
+        let i = xs.hit();
+        assert_eq!(*i.unwrap(), i1);
+
+        // the hit when intersections have negative t
+        let i1 = Intersection::new(-1.0, s.clone());
+        let i2 = Intersection::new(1.0, s.clone());
+        let mut xs = vec![i2, i1];
+        xs.sort_by(|a, b| a.t.partial_cmp(&b.t).unwrap());
+        let i = xs.hit();
+        assert_eq!(*i.unwrap(), i2);
+
+        // the hit when all intersections have negative t
+        let i1 = Intersection::new(-2.0, s.clone());
+        let i2 = Intersection::new(-1.0, s.clone());
+        let mut xs = vec![i2, i1];
+        xs.sort_by(|a, b| a.t.partial_cmp(&b.t).unwrap());
+        let i = xs.hit();
+        assert_eq!(i, None);
+
+        // the hit is always the lowest non-negative intersection
+        let i1 = Intersection::new(5.0, s.clone());
+        let i2 = Intersection::new(7.0, s.clone());
+        let i3 = Intersection::new(-3.0, s.clone());
+        let i4 = Intersection::new(2.0, s.clone());
+        let mut xs = vec![i1, i2, i3, i4];
+        xs.sort_by(|a, b| a.t.partial_cmp(&b.t).unwrap());
+        let i = xs.hit();
+        assert_eq!(*i.unwrap(), i4);
     }
 }
