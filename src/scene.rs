36e168eff3fd1d7c83fd901ff24ddf52562a946e
Joshua Benuck <joshua_benuck@yahoo.com>
2020-08-27 18:39:03 -0700

    add Camera

--- src/scene.rs
+++ src/scene.rs
@@ -1,6 +1,6 @@
 use crate::{
-    pt, v, Color, Comps, Intersection, Intersections, Material, Matrix4x4, PointLight, Ray, Sphere,
-    Tuple,
+    pt, v, Canvas, Color, Comps, Intersection, Intersections, Material, Matrix4x4, PointLight, Ray,
+    Sphere, Tuple,
 };
 
 pub struct World {
@@ -83,9 +83,86 @@ pub fn view_transform(from: Tuple, to: Tuple, up: Tuple) -> Matrix4x4 {
     orientation * Matrix4x4::translation(-from.x, -from.y, -from.z)
 }
 
+struct Camera {
+    hsize: usize,
+    vsize: usize,
+    field_of_view: f64,
+    transform: Matrix4x4,
+    pixel_size: f64,
+    half_width: f64,
+    half_height: f64,
+}
+
+impl Camera {
+    fn new(hsize: usize, vsize: usize, field_of_view: f64) -> Camera {
+        let hsize = hsize as f64;
+        let vsize = vsize as f64;
+        let half_view = (field_of_view / 2.0).tan();
+        let aspect = hsize / vsize;
+
+        let half_width;
+        let half_height;
+        if aspect >= 1.0 {
+            half_width = half_view;
+            half_height = half_view / aspect;
+        } else {
+            half_width = half_view * aspect;
+            half_height = half_view;
+        }
+
+        let pixel_size = (half_width * 2.0) / hsize;
+
+        Camera {
+            hsize: hsize as usize,
+            vsize: vsize as usize,
+            field_of_view,
+            transform: Matrix4x4::identity(),
+            half_width,
+            half_height,
+            pixel_size,
+        }
+    }
+
+    pub fn ray_for_pixel(&self, px: usize, py: usize) -> Ray {
+        let px = px as f64;
+        let py = py as f64;
+        // the offset from the edge of the canvas to the pixel's center
+        let xoffset = (px + 0.5) * self.pixel_size;
+        let yoffset = (py + 0.5) * self.pixel_size;
+
+        // the untransformed coordinates of the pixel in world space
+        // remember that the camera looks toward -z, so +x is to the *left*
+        let world_x = self.half_width - xoffset;
+        let world_y = self.half_height - yoffset;
+
+        // using the camera matrix, transform the canvas point and the origin
+        // and then compute the ray's direction vector
+        let pixel = self.transform.inverse().unwrap() * pt(world_x, world_y, -1.0);
+        let origin = self.transform.inverse().unwrap() * pt(0.0, 0.0, 0.0);
+        let direction = (pixel - origin).normalize();
+
+        Ray::new(origin, direction)
+    }
+
+    pub fn render(&self, world: &World) -> Canvas {
+        let mut image = Canvas::new(self.hsize, self.vsize);
+
+        for y in 0..self.vsize {
+            for x in 0..self.hsize {
+                let ray = self.ray_for_pixel(x, y);
+                let color = world.color_at(&ray);
+                image.write_pixel(x, y, color);
+            }
+        }
+        image
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::f64::consts::PI;
+
     #[test]
     fn world_create() {
         // creating a world
@@ -207,5 +284,68 @@ mod tests {
                 [0.0, 0.0, 0.0, 1.0]
             ])
         );
+    }
+
+    const EPSILON: f64 = 0.00001;
+
+    fn equal(a: f64, b: f64) -> bool {
+        f64::abs(a - b) < EPSILON
+    }
+
+    #[test]
+    fn camera_create() {
+        // constructing a camera
+        let hsize = 160;
+        let vsize = 120;
+        let field_of_view = PI / 2.0;
+        let c = Camera::new(hsize, vsize, field_of_view);
+        assert_eq!(c.hsize, hsize);
+        assert_eq!(c.vsize, vsize);
+        assert_eq!(c.field_of_view, PI / 2.0);
+        assert_eq!(c.transform, Matrix4x4::identity());
+
+        // the pixel size for a horizontal canvas
+        let c = Camera::new(200, 125, PI / 2.0);
+        assert_eq!(equal(c.pixel_size, 0.01), true);
+
+        // the pixel size for a vertical canvas
+        let c = Camera::new(125, 200, PI / 2.0);
+        assert_eq!(equal(c.pixel_size, 0.01), true);
+    }
+
+    #[test]
+    fn camera_ray_for_pixel() {
+        // constructing a ray through the center of the canvas
+        let mut c = Camera::new(201, 101, PI / 2.0);
+        let r = c.ray_for_pixel(100, 50);
+        assert_eq!(r.origin, pt(0.0, 0.0, 0.0));
+        assert_eq!(r.direction, v(0.0, 0.0, -1.0));
+
+        // constructing a ray through a corner of the canvas
+        let r = c.ray_for_pixel(0, 0);
+        assert_eq!(r.origin, pt(0.0, 0.0, 0.0));
+        assert_eq!(r.direction, v(0.66519, 0.33259, -0.66851));
+
+        // constructing a ray when the camera is transformed
+        c.transform = Matrix4x4::rotation_y(PI / 4.0) * Matrix4x4::translation(0.0, -2.0, 5.0);
+        let r = c.ray_for_pixel(100, 50);
+        assert_eq!(r.origin, pt(0.0, 2.0, -5.0));
+        assert_eq!(
+            r.direction,
+            v(2.0_f64.sqrt() / 2.0, 0.0, -2.0_f64.sqrt() / 2.0)
+        );
+    }
+
+    #[test]
+    fn camera_render() {
+        // rendering a world with a camera
+        let w = World::default();
+        let mut c = Camera::new(11, 11, PI / 2.0);
+        let from = pt(0.0, 0.0, -5.0);
+        let to = pt(0.0, 0.0, 0.0);
+        let up = v(0.0, 1.0, 0.0);
+        c.transform = view_transform(from, to, up);
+        let image = c.render(&w);
+        assert_eq!(image.pixel_at(5, 5), Color::new(0.38066, 0.47583, 0.2855));
     }
 }
