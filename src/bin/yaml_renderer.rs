dc4c9ac87759f3a799ed817d78f30b9e4b5aebdb
Joshua Benuck <joshua_benuck@yahoo.com>
2020-09-09 22:16:00 -0700

    yaml scene renderer now renders cover image

--- src/bin/yaml_renderer.rs
+++ src/bin/yaml_renderer.rs
@@ -27,45 +27,53 @@ fn to_color(obj: &Yaml) -> Result<Color> {
     ))
 }
 
-fn apply_transfor(base: Matrix4x4, params: &Yaml) {}
-
-fn to_transform(transforms: &Vec<Yaml>) -> Result<Matrix4x4> {
-    let mut transform = id();
-    for params in transforms {
-        let transform_type = params[0].as_str().unwrap();
-        match transform_type {
-            "rotate-x" => {
-                transform = transform.rotate_x(to_f64(&params[1])?);
-            }
-            "rotate-y" => {
-                transform = transform.rotate_y(to_f64(&params[1])?);
-            }
-            "rotate-z" => {
-                transform = transform.rotate_z(to_f64(&params[1])?);
-            }
-            "scale" => {
-                transform = transform.scale(
-                    to_f64(&params[1])?,
-                    to_f64(&params[2])?,
-                    to_f64(&params[3])?,
-                );
-            }
-            "translate" => {
-                transform = transform.translate(
-                    to_f64(&params[1])?,
-                    to_f64(&params[2])?,
-                    to_f64(&params[3])?,
-                );
-            }
-            _ => {
-                panic!("Unrecognized plane transform: {}", transform_type);
-            }
+fn apply_transform(transform: Matrix4x4, params: &Yaml) -> Result<Matrix4x4> {
+    let mut transform = transform;
+    let transform_type = params[0].as_str().unwrap();
+    match transform_type {
+        "rotate-x" => {
+            transform = transform.rotate_x(to_f64(&params[1])?);
+        }
+        "rotate-y" => {
+            transform = transform.rotate_y(to_f64(&params[1])?);
+        }
+        "rotate-z" => {
+            transform = transform.rotate_z(to_f64(&params[1])?);
+        }
+        "scale" => {
+            transform = transform.scale(
+                to_f64(&params[1])?,
+                to_f64(&params[2])?,
+                to_f64(&params[3])?,
+            );
+        }
+        "translate" => {
+            transform = transform.translate(
+                to_f64(&params[1])?,
+                to_f64(&params[2])?,
+                to_f64(&params[3])?,
+            );
+        }
+        _ => {
+            panic!("Unrecognized plane transform: {}", transform_type);
         }
     }
     Ok(transform)
 }
 
-fn to_material(obj: &Yaml) -> Result<Material> {
+fn to_transform(transforms: &Vec<Yaml>, defines: &HashMap<String, Matrix4x4>) -> Result<Matrix4x4> {
+    let mut transform = id();
+    for params in transforms {
+        match &params {
+            Yaml::Array(_) => transform = apply_transform(transform, params)?,
+            Yaml::String(name) => transform = defines[name],
+            _ => unreachable!(),
+        }
+    }
+    Ok(transform)
+}
+
+fn to_material(obj: &Yaml, transforms: &HashMap<String, Matrix4x4>) -> Result<Material> {
     let mut material = Material::new();
     let props = &obj["pattern"];
     if props != &Yaml::BadValue {
@@ -84,8 +92,8 @@ fn to_material(obj: &Yaml) -> Result<Material> {
                     let a = to_color(&colors[0])?;
                     let b = to_color(&colors[1])?;
                     let mut pattern = stripe_pattern(a, b);
-                    if let Yaml::Array(transforms) = &props["transform"] {
-                        pattern.transform = to_transform(transforms)?;
+                    if let Yaml::Array(ts) = &props["transform"] {
+                        pattern.transform = to_transform(ts, &transforms)?;
                     }
                     material.pattern = Some(pattern);
                 }
@@ -130,6 +138,7 @@ fn main() -> Result<()> {
     let mut transforms: HashMap<String, Matrix4x4> = HashMap::new();
     for obj in scene[0].as_vec().unwrap() {
         if let Yaml::String(r#type) = &obj["add"] {
+            println!("Adding {}", r#type);
             match r#type.as_str() {
                 "camera" => {
                     //   width: 400
@@ -150,54 +159,49 @@ fn main() -> Result<()> {
                     camera = Some(c);
                 }
                 "light" => {
-                    //   at: [-4.9, 4.9, -1]
-                    //   intensity: [1, 1, 1]
                     let at = to_pt(&obj["at"])?;
                     let intensity = to_color(&obj["intensity"])?;
                     let light = PointLight::new(at, intensity);
                     world.lights.push(light);
                 }
+                "cube" => {
+                    let mut cube = Cube::new();
+                    match &obj["transform"] {
+                        Yaml::Array(ts) => cube.set_transform(to_transform(ts, &transforms)?),
+                        Yaml::String(name) => cube.set_transform(transforms[name].clone()),
+                        _ => {}
+                    }
+                    let props = &obj["material"];
+                    match props {
+                        Yaml::Hash(_) => cube.set_material(to_material(&props, &transforms)?),
+                        Yaml::String(name) => cube.set_material(materials[name].clone()),
+                        _ => {}
+                    }
+                    world.objects.push(cube.shape());
+                }
                 "plane" => {
-                    // - add: plane
-                    //   transform:
-                    //     - [rotate-y, 0.31415]
                     let mut plane = Plane::new();
                     match &obj["transform"] {
-                        Yaml::Array(transforms) => plane.set_transform(to_transform(transforms)?),
+                        Yaml::Array(ts) => plane.set_transform(to_transform(ts, &transforms)?),
                         Yaml::String(name) => plane.set_transform(transforms[name].clone()),
                         _ => {}
                     }
-                    //   material:
-                    //     pattern:
-                    //       type: checkers
-                    //       colors:
-                    //         - [0.35, 0.35, 0.35]
-                    //         - [0.65, 0.65, 0.65]
-                    //     specular: 0
-                    //     reflective: 0.4
                     let props = &obj["material"];
                     match props {
-                        Yaml::Hash(_) => plane.set_material(to_material(&props)?),
+                        Yaml::Hash(_) => plane.set_material(to_material(&props, &transforms)?),
                         Yaml::String(name) => plane.set_material(materials[name].clone()),
                         _ => {}
                     }
                     world.objects.push(plane.shape());
                 }
                 "sphere" => {
-                    // - add: sphere
-                    //   transform:
-                    //     - [scale, 0.4, 0.4, 0.4]
-                    //     - [translate, 4.6, 0.4, 1]
-                    //   material:
-                    //     color: [0.8, 0.5, 0.3]
-                    //     shininess: 50
                     let mut sphere = Sphere::new();
-                    if let Yaml::Array(transforms) = &obj["transform"] {
-                        sphere.set_transform(to_transform(transforms)?);
+                    if let Yaml::Array(ts) = &obj["transform"] {
+                        sphere.set_transform(to_transform(ts, &transforms)?);
                     }
                     let props = &obj["material"];
                     if props != &Yaml::BadValue {
-                        sphere.set_material(to_material(&props)?);
+                        sphere.set_material(to_material(&props, &transforms)?);
                     }
                     world.objects.push(sphere.shape());
                 }
@@ -207,11 +211,12 @@ fn main() -> Result<()> {
             }
         }
         if let Yaml::String(name) = &obj["define"] {
+            println!("Defining {}", name);
             if name.contains("-material") {
-                materials.insert(name.clone(), to_material(&obj["value"])?);
+                materials.insert(name.clone(), to_material(&obj["value"], &transforms)?);
             } else if name.contains("-transform") {
                 if let Yaml::Array(value) = &obj["value"] {
-                    transforms.insert(name.clone(), to_transform(&value)?);
+                    transforms.insert(name.clone(), to_transform(&value, &transforms)?);
                 } else {
                     panic!("Unexpected transform define structure!");
                 }
@@ -219,6 +224,8 @@ fn main() -> Result<()> {
                 if let Yaml::Array(value) = &obj["value"] {
                     let base = value[0].as_str().unwrap();
                     let mut transform = transforms[base].clone();
+                    transform = apply_transform(transform, &value[1])?;
+                    transforms.insert(name.clone(), transform);
                 }
             } else {
                 panic!("Unexpected define type: {}!", name);
@@ -228,6 +235,7 @@ fn main() -> Result<()> {
 
     match camera {
         Some(camera) => {
+            println!("Rendering");
             let image = camera.render(&mut world);
             let path = std::path::PathBuf::from(path);
             let image_base = path.file_stem().unwrap().to_str().unwrap();
