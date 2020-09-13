90917452d60eb1112c358351a4148ae005bed59c
Joshua Benuck <joshua_benuck@yahoo.com>
2020-09-12 22:26:02 -0700

    finish refactor of yaml scene renderer

--- src/bin/yaml_renderer.rs
+++ src/bin/yaml_renderer.rs
@@ -19,7 +19,7 @@ impl Definitions {
     }
 
     fn define(&mut self, obj: &Yaml) -> Result<()> {
-        let name = obj["define"].as_str().unwrap();
+        let name = obj["define"].as_str().expect("define is not a String");
         println!("Defining {}", name);
         if name.contains("leg") || name.contains("cap") || name.contains("wacky") {
             let value = &obj["value"];
@@ -29,14 +29,16 @@ impl Definitions {
             if let Some(extend) = obj["extend"].as_str() {
                 base = self.materials[extend].clone();
             }
-            self.materials
-                .insert(name.to_owned(), apply_material(base, &obj["value"], &self)?);
+            self.materials.insert(
+                name.to_owned(),
+                obj["value"].populate_material(base, &self)?,
+            );
         } else if name.contains("-transform") {
             self.transforms
                 .insert(name.to_owned(), obj["value"].as_transform(&self)?);
         } else if name.contains("-object") {
             if let Yaml::Array(value) = &obj["value"] {
-                let base = value[0].as_str().unwrap();
+                let base = value[0].as_str().expect("base object name not a String");
                 let mut transform = self.transforms[base].clone();
                 transform = value[1].populate_transform(transform)?;
                 self.transforms.insert(name.to_owned(), transform);
@@ -57,13 +59,20 @@ trait YamlExt {
     fn as_light(&self) -> Result<PointLight>;
     fn as_transform(&self, defs: &Definitions) -> Result<Matrix4x4>;
     fn populate_transform(&self, transform: Matrix4x4) -> Result<Matrix4x4>;
+    fn as_material(&self, defs: &Definitions) -> Result<Material>;
+    fn populate_material(&self, material: Material, defs: &Definitions) -> Result<Material>;
+    fn as_shape(&self, defs: &Definitions) -> Result<Box<dyn Shape>>;
+    fn populate_shape(&self, shape: Box<dyn Shape>, defs: &Definitions) -> Result<Box<dyn Shape>>;
+    fn pattern_props(&self, defs: &Definitions) -> Result<(Color, Color, Matrix4x4)>;
 }
 
 impl YamlExt for Yaml {
     fn as_float(&self) -> Result<f64> {
         match &self {
-            Yaml::Real(_) => Ok(self.as_f64().unwrap()),
-            Yaml::Integer(_) => Ok(self.as_i64().unwrap() as f64),
+            Yaml::Real(_) => Ok(self.as_f64().expect("Unable to extract f64 from Real")),
+            Yaml::Integer(_) => {
+                Ok(self.as_i64().expect("Unable to extract i64 from Integer") as f64)
+            }
             _ => Err(anyhow!("Unable to read f64")),
         }
     }
@@ -93,26 +102,33 @@ impl YamlExt for Yaml {
     }
 
     fn as_camera(&self) -> Result<Camera> {
-        let mut camera = Camera::new(0, 0, 0.0);
+        let mut width = None;
+        let mut height = None;
+        let mut field_of_view = None;
         let mut from = None;
         let mut to = None;
         let mut up = None;
-        let hash = self.as_hash().unwrap();
+        let hash = self.as_hash().expect("Camera properties are not a Hash");
         for (key, value) in hash.iter() {
             let key = key
                 .as_str()
                 .expect("Unexpected key type for camera property");
             match key {
                 "add" => {}
-                "width" => camera.hsize = value.as_float()? as usize,
-                "height" => camera.vsize = value.as_float()? as usize,
-                "field-of-view" => camera.field_of_view = value.as_float()?,
+                "width" => width = Some(value.as_float()? as usize),
+                "height" => height = Some(value.as_float()? as usize),
+                "field-of-view" => field_of_view = Some(value.as_float()?),
                 "from" => from = Some(value.as_v()?),
                 "to" => to = Some(value.as_v()?),
                 "up" => up = Some(value.as_v()?),
                 _ => return Err(anyhow!("Unexpected camera property: {}", key)),
             }
         }
+        let mut camera = Camera::new(
+            width.expect("camera missing required field 'width'"),
+            height.expect("camera missing required field 'height"),
+            field_of_view.expect("camera missing required field 'field-of-view'"),
+        );
         camera.transform(view_transform(
             from.expect("camera missing required field 'from'"),
             to.expect("camera missing required field 'to'"),
@@ -123,7 +139,7 @@ impl YamlExt for Yaml {
 
     fn as_light(&self) -> Result<PointLight> {
         let mut light = PointLight::default();
-        let hash = self.as_hash().unwrap();
+        let hash = self.as_hash().expect("light properties are not a Hash");
         for (key, value) in hash.iter() {
             let key = key
                 .as_str()
@@ -152,7 +168,7 @@ impl YamlExt for Yaml {
 
     fn populate_transform(&self, transform: Matrix4x4) -> Result<Matrix4x4> {
         let mut transform = transform;
-        let transform_type = self[0].as_str().unwrap();
+        let transform_type = self[0].as_str().expect("tranform type is not a String");
         match transform_type {
             "rotate-x" => {
                 transform = transform.rotate_x(self[1].as_float()?);
@@ -182,6 +198,163 @@ impl YamlExt for Yaml {
             }
         }
         Ok(transform)
+    }
+
+    fn as_material(&self, defs: &Definitions) -> Result<Material> {
+        self.populate_material(Material::new(), defs)
+    }
+
+    fn populate_material(&self, material: Material, defs: &Definitions) -> Result<Material> {
+        let mut material = material;
+        for (key, value) in self
+            .as_hash()
+            .expect("material properties are not a Hash")
+            .iter()
+        {
+            let key = key
+                .as_str()
+                .expect("Unexpected key type for pattern property");
+            match key {
+                "pattern" => {
+                    let r#type = value["type"]
+                        .as_str()
+                        .expect("pattern type is not a String");
+                    match r#type {
+                        "checkers" => {
+                            let (a, b, transform) = value.pattern_props(&defs)?;
+                            let mut pattern = checkers_pattern(a, b);
+                            pattern.transform = transform;
+                            material.pattern = Some(pattern);
+                        }
+                        "stripes" => {
+                            let (a, b, transform) = value.pattern_props(&defs)?;
+                            let mut pattern = stripe_pattern(a, b);
+                            pattern.transform = transform;
+                            material.pattern = Some(pattern);
+                        }
+                        _ => panic!("Unexpected pattern type: {}", r#type),
+                    }
+                }
+                "color" => material.color = value.as_color()?,
+                "ambient" => material.ambient = value.as_float()?,
+                "diffuse" => material.diffuse = value.as_float()?,
+                "specular" => material.specular = value.as_float()?,
+                "reflective" => material.reflective = value.as_float()?,
+                "shininess" => material.shininess = value.as_float()?,
+                "transparency" => material.transparency = value.as_float()?,
+                "refractive-index" => material.refractive_index = value.as_float()?,
+                _ => {
+                    return Err(anyhow!("Unknown material property: {}", key));
+                }
+            }
+        }
+        Ok(material)
+    }
+
+    fn as_shape(&self, defs: &Definitions) -> Result<Box<dyn Shape>> {
+        let r#type = self["add"].as_str().expect("add parameter is not a String");
+        println!("Adding {}", r#type);
+        let shape = match r#type {
+            "cube" => {
+                let mut shape = Cube::new().shape();
+                shape = self.populate_shape(shape, defs)?;
+                shape
+            }
+            "plane" => {
+                let mut shape = Plane::new().shape();
+                shape = self.populate_shape(shape, defs)?;
+                shape
+            }
+            "sphere" => {
+                let mut shape = Sphere::new().shape();
+                shape = self.populate_shape(shape, defs)?;
+                shape
+            }
+            "cylinder" => {
+                let min = self["min"].as_float()?;
+                let max = self["max"].as_float()?;
+                let closed = self["closed"]
+                    .as_bool()
+                    .expect("closed property is not a Bool");
+                let mut shape = Cylinder::new(min, max, closed).shape();
+                shape = self.populate_shape(shape, defs)?;
+                shape
+            }
+            "cone" => {
+                let min = self["min"].as_float()?;
+                let max = self["max"].as_float()?;
+                let closed = self["closed"]
+                    .as_bool()
+                    .expect("close property is not a Bool");
+                let mut shape = Cone::new(min, max, closed).shape();
+                shape = self.populate_shape(shape, defs)?;
+                shape
+            }
+            "group" => {
+                let mut group = Group::new();
+                for child_obj in self["children"]
+                    .as_vec()
+                    .expect("children property not an Array")
+                {
+                    let child = child_obj.as_shape(defs)?;
+                    group.add_child(child);
+                }
+                let mut shape = group.shape();
+                shape = self.populate_shape(shape, defs)?;
+                shape
+            }
+            name => {
+                if let Some(def) = defs.shapes.get(name) {
+                    let mut shape = def.clone().as_shape(defs)?;
+                    shape = self.populate_shape(shape, defs)?;
+                    shape
+                } else {
+                    panic!("Unknown shape: {}", name);
+                }
+            }
+        };
+        Ok(shape)
+    }
+
+    fn populate_shape(&self, shape: Box<dyn Shape>, defs: &Definitions) -> Result<Box<dyn Shape>> {
+        let mut shape = shape;
+        match &self["transform"] {
+            Yaml::Array(_) => shape.set_transform(self["transform"].as_transform(&defs)?),
+            Yaml::String(name) => shape.set_transform(defs.transforms[name].clone()),
+            _ => {}
+        }
+        match &self["material"] {
+            Yaml::Hash(_) => shape.set_material(self["material"].as_material(defs)?),
+            Yaml::String(name) => shape.set_material(defs.materials[name].clone()),
+            _ => {}
+        }
+        if let Some(shadow) = self["shadow"].as_bool() {
+            shape.set_shadow(shadow);
+        }
+        Ok(shape)
+    }
+
+    fn pattern_props(&self, defs: &Definitions) -> Result<(Color, Color, Matrix4x4)> {
+        let mut a = Color::default();
+        let mut b = Color::default();
+        let mut transform = Matrix4x4::default();
+        for (key, value) in self.as_hash().unwrap().iter() {
+            let key = key
+                .as_str()
+                .expect("Unexpected key type for pattern property");
+            match key {
+                "type" => {}
+                "colors" => {
+                    a = value[0].as_color()?;
+                    b = value[1].as_color()?;
+                }
+                "transform" => {
+                    transform = value.as_transform(defs)?;
+                }
+                _ => return Err(anyhow!("Unexpected pattern property: {}", key)),
+            }
+        }
+        Ok((a, b, transform))
     }
 }
 
@@ -214,12 +387,8 @@ impl YamlScene {
                     "light" => {
                         self.world.lights.push(obj.as_light()?);
                     }
-                    "group" | "cube" | "plane" | "sphere" | "cylinder" | "cone" | "wacky"
-                    | "cap" | "leg" => {
-                        self.world.objects.push(add_shape(&obj, &self.definitions)?);
-                    }
                     _ => {
-                        println!("Uknown object type: {}", r#type);
+                        self.world.objects.push(obj.as_shape(&self.definitions)?);
                     }
                 }
             } else if let Yaml::String(_) = &obj["define"] {
@@ -251,156 +420,13 @@ impl YamlScene {
     }
 }
 
-fn pattern_props(obj: &Yaml, defs: &Definitions) -> Result<(Color, Color, Matrix4x4)> {
-    let mut a = Color::default();
-    let mut b = Color::default();
-    let mut transform = Matrix4x4::default();
-    for (key, value) in obj.as_hash().unwrap().iter() {
-        let key = key
-            .as_str()
-            .expect("Unexpected key type for pattern property");
-        match key {
-            "colors" => {
-                a = value[0].as_color()?;
-                b = value[1].as_color()?;
-            }
-            "transform" => {
-                transform = value.as_transform(defs)?;
-            }
-            _ => return Err(anyhow!("Unexpected pattern property: {}", key)),
-        }
-    }
-    Ok((a, b, transform))
-}
-
-fn apply_material(material: Material, obj: &Yaml, defs: &Definitions) -> Result<Material> {
-    let mut material = material;
-    for (key, value) in obj.as_hash().unwrap().iter() {
-        let key = key
-            .as_str()
-            .expect("Unexpected key type for pattern property");
-        match key {
-            "pattern" => {
-                let r#type = value["type"].as_str().unwrap();
-                match r#type {
-                    "checkers" => {
-                        let (a, b, transform) = pattern_props(value, &defs)?;
-                        let mut pattern = checkers_pattern(a, b);
-                        pattern.transform = transform;
-                        material.pattern = Some(pattern);
-                    }
-                    "stripes" => {
-                        let (a, b, transform) = pattern_props(value, &defs)?;
-                        let mut pattern = stripe_pattern(a, b);
-                        pattern.transform = transform;
-                        material.pattern = Some(pattern);
-                    }
-                    _ => panic!("Unexpected pattern type: {}", r#type),
-                }
-            }
-            "color" => material.color = value.as_color()?,
-            "ambient" => material.ambient = value.as_float()?,
-            "diffuse" => material.diffuse = value.as_float()?,
-            "specular" => material.specular = value.as_float()?,
-            "reflective" => material.reflective = value.as_float()?,
-            "shininess" => material.shininess = value.as_float()?,
-            "transparency" => material.transparency = value.as_float()?,
-            "refractive-index" => material.refractive_index = value.as_float()?,
-            _ => {
-                return Err(anyhow!("Unknown material property: {}", key));
-            }
-        }
-    }
-    Ok(material)
-}
-
-fn to_material(obj: &Yaml, defs: &Definitions) -> Result<Material> {
-    apply_material(Material::new(), obj, defs)
-}
-
-fn apply_shape(shape: &mut dyn Shape, obj: &Yaml, defs: &Definitions) -> Result<()> {
-    match &obj["transform"] {
-        Yaml::Array(_) => shape.set_transform(obj["transform"].as_transform(&defs)?),
-        Yaml::String(name) => shape.set_transform(defs.transforms[name].clone()),
-        _ => {}
-    }
-    let props = &obj["material"];
-    match props {
-        Yaml::Hash(_) => shape.set_material(to_material(&props, defs)?),
-        Yaml::String(name) => shape.set_material(defs.materials[name].clone()),
-        _ => {}
-    }
-    if let Some(shadow) = &obj["shadow"].as_bool() {
-        shape.set_shadow(*shadow);
-    }
-    Ok(())
-}
-
-fn add_shape(obj: &Yaml, defs: &Definitions) -> Result<Box<dyn Shape>> {
-    let r#type = obj["add"].as_str().unwrap();
-    println!("Adding {}", r#type);
-    let shape = match r#type {
-        "cube" => {
-            let mut cube = Cube::new().shape();
-            apply_shape(&mut *cube, &obj, defs)?;
-            cube
-        }
-        "plane" => {
-            let mut plane = Plane::new().shape();
-            apply_shape(&mut *plane, &obj, defs)?;
-            plane
-        }
-        "sphere" => {
-            let mut sphere = Sphere::new().shape();
-            apply_shape(&mut *sphere, &obj, defs)?;
-            sphere
-        }
-        "cylinder" => {
-            let min = obj["min"].as_float()?;
-            let max = obj["max"].as_float()?;
-            let closed = obj["closed"].as_bool().unwrap();
-            let mut cylinder = Cylinder::new(min, max, closed).shape();
-            apply_shape(&mut *cylinder, &obj, defs)?;
-            cylinder
-        }
-        "cone" => {
-            let min = obj["min"].as_float()?;
-            let max = obj["max"].as_float()?;
-            let closed = obj["closed"].as_bool().unwrap();
-            let mut cone = Cone::new(min, max, closed).shape();
-            apply_shape(&mut *cone, &obj, defs)?;
-            cone
-        }
-        "group" => {
-            let mut group = Group::new();
-            for child_obj in obj["children"].as_vec().unwrap() {
-                let child = add_shape(child_obj, defs)?;
-                group.add_child(child);
-            }
-            let mut group = group.shape();
-            apply_shape(&mut *group, &obj, defs)?;
-            group
-        }
-        name => {
-            if let Some(def) = defs.shapes.get(name) {
-                let mut shape = add_shape(&def.clone(), defs)?;
-                apply_shape(&mut *shape, &obj, defs)?;
-                shape
-            } else {
-                panic!("Unknown shape: {}", name);
-            }
-        }
-    };
-    Ok(shape)
-}
-
 fn main() -> Result<()> {
     let path = std::env::args().nth(1).expect("no yaml file provided");
     let mut scene = YamlScene::new(&path)?;
     scene.parse()?;
 
     let image = scene.render();
-    // scene.save(&path, image)?;
+    scene.save(&path, image)?;
 
     Ok(())
 }
